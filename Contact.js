538fe37643702683386f8f244339a1d5ac855d66
nikita12012005 <115627001+nikita12012005@users.noreply.github.com>
2023-05-29 23:15:41 +0300

    Create Contact.js

---
+++ Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h2>Контакти</h2>
+      <p>Пошта: example@example.com</p>
+      <p>Телефон: 123-456-7890</p>
+      <p>Додаткова інформація</p>
+    </div>
+  );
+};
+
+export default Contact;
